55d3723899689bcbaa1c9d70898b742557236f6d
desam47 <dipeshchand747@gmail.com>
2022-05-23 13:05:27 -0400

    updating files

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -2,6 +2,24 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture()
-def setup():
-    driver=webdriver.Chrome()
+def setup(browser):
+    if browser=='chrome':
+        driver=webdriver.Chrome()
+        print("Launching Chrome browser.......")
+
+    elif browser=='firefox':
+        driver=webdriver.Firefox()
+        print("Launching Firefox browser.......")
+
+    else:
+        driver = webdriver.Chrome()
+        print("Launching default browser.......")
+
     return driver
+
+def pytest_addoption(parser):  #This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+@pytest.fixture()
+def browser(request):    #This will return the Browser value to setup method
+    return request.config.getoption("--browser")

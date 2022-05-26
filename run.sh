e8e1a70cb10692a1b881c6d851e6432cf140cfdb
desam47 <dipeshchand747@gmail.com>
2022-05-25 22:32:15 -0400

    Updating run.sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-#echo "Hello world"
 ./venv/bin/pytest -s -v -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 #/home/dips/pythonProject/nopcommerceApp/venv/bin/pytest -v -m "sanity or regression" --html=Reports/report.html testCases/ --browser chrome
 #/home/dips/pythonProject/nopcommerceApp/venv/bin/pytest -v -m "sanity and regression" --html=Reports/report.html testCases/ --browser chrome
